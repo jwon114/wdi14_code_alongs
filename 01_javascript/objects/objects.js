33bcf0d255b5b21f8c7481426cc748d2c1a3bca4
jwon114 <jthgwong@gmail.com>
2017-11-30 14:43:51 +1100

    loops

---
+++ 01_javascript/objects/objects.js
@@ -0,0 +1,18 @@
+console.log('objects');
+
+var student = {
+	name: 'cat empire',
+	'bank balance': 1000
+}
+
+var wdi14 = {
+	name: 'byteme',
+	campus: 'melbourne',
+	students: ['robbie', 'alex'],
+	alex: { 
+		balance: 1000
+	},
+	studentsCount: 23
+}
+
+console.log(wdi14);
