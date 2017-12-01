af0657bce22a97fc1e93635fdd4edd2c4b898595
jwon114 <jthgwong@gmail.com>
2017-12-01 16:01:17 +1100

    solve_my_problem grouping

--- 01_javascript/solve_my_problem/grouping.js
+++ 01_javascript/solve_my_problem/grouping.js
@@ -2,50 +2,51 @@ var people = ['Alex', 'James', 'Robbie', 'Anushka', 'Ross', 'Aysa', 'Joanna Phil
 'Chang', 'Johanna Schreiner', 'Sheila', 'Emma', 'Karen',
 'Sherry', 'Ethan', 'Mark', 'Sophie', 'Fouad', 'Mia', 'Vivien', 'Haofu', 'Wally'];
 var scrambled = [];
-var group_size = 5;
-// var group_size = prompt('Enter the group size');
+var group_size = 6;
 var valid = false;
 
-// if (group_size.match(/\d+/) && group_size <= people.length) {
-// 	valid = true;
-// }
-
 // scramble the array of people
-var scramble = function(people) {
+var scrambleArray = function(people) {
+	var newArr = [];
 	while (people.length > 0) {
 		var random = Math.floor((Math.random() * people.length));
 		var person = people[random];
-		scrambled.push(person);
+		newArr.push(person);
 		people.splice(random,1);
 	}
+	return newArr
 }
 
-// if (valid) {
-	scramble(people);
+scrambled = scrambleArray(people);
 
-	var groups = [];
-	if (scrambled.length % group_size === 0) {
-		for (var i = 0; i < scrambled.length; i+=group_size) {
-			var sub_group = scrambled.slice(i, i + group_size);
-			groups.push(sub_group);
-		}
-	} else {
-		for (var i = 0; i < scrambled.length; i+=group_size) {
-			var sub_group = scrambled.slice(i, i + group_size);
-			groups.push(sub_group);
-		}
-		var leftovers = groups.pop();
-		console.log('leftover', leftovers);
-
-		for (var l = 0; l < leftovers.length; l++) {
-			var randomGroup = Math.floor((Math.random() * groups.length));
-			groups[randomGroup].push(leftovers[l]);
-		}
-
+var groups = [];
+if (scrambled.length % group_size === 0) {
+	for (var i = 0; i < scrambled.length; i+=group_size) {
+		var sub_group = scrambled.slice(i, i + group_size);
+		groups.push(sub_group);
+	}
+} else {
+	for (var i = 0; i < scrambled.length; i+=group_size) {
+		var sub_group = scrambled.slice(i, i + group_size);
+		groups.push(sub_group);
 	}
 
-	// console.log('scrambled people', scrambled);
-	console.log('groups', groups);
-// } else {
-// 	group_size = prompt('Not a valid group size!');
-// }
+	// pop the last sub array and assign to leftovers
+	var leftovers = groups.pop();
+
+	// reassign remaining leftover people to the groups spread as evenly as possible
+	var newGroup = 0;
+	for (var l = 0; l < leftovers.length; l++) {
+		groups[newGroup].push(leftovers[l]);
+		(newGroup < groups.length - 1) ? newGroup++ : newGroup = 0;
+	}
+
+    // for (var l = 0; l < leftovers.length; l++) {
+    //     var randomGroup = Math.floor((Math.random() * groups.length));
+    //     groups[randomGroup].push(leftovers[l]);
+    // }
+
+}
+
+// console.log('scrambled people', scrambled);
+console.log('groups', groups);
